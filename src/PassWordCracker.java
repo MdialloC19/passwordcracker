c7bd69f2080525d07e5cd2a415952d6211a8dbc8
MdialloC19 <mdiallo3211@gmail.com>
2023-05-18 04:42:03 +0000

    Génération des combinaisons effectuer

--- src/PassWordCracker.java
+++ src/PassWordCracker.java
@@ -4,5 +4,25 @@ public class PassWordCracker {
     
     public static void main(String []args){
         System.out.println("****** Programme pour casser un code de 5 à 6 chiffres par deux méthodes ********\n\t\t1- Méthode par BRUTE FORCE \n\t\t2- Méthode par DICTIONNAIRE");
+    
+        int passwordLength = 6; // Longueur du mot de passe
+        char[] password = new char[passwordLength];
+
+        while (password[passwordLength - 1] < 'z') {
+            // Afficher le mot de passe actuel
+            System.out.println(new String(password));
+
+            // Générer la prochaine combinaison
+            int index = 0;
+            while (index < passwordLength) {
+                if (password[index] < 'z') {
+                    password[index]++;
+                    break;
+                } else {
+                    password[index] = 'a';
+                    index++;
+                }
+            }
+        }
     }
 }
--- src/com/methods/BruteForce.java
+++ src/com/methods/BruteForce.java
@@ -2,7 +2,12 @@ package com.methods;
 
 public class BruteForce extends Methodes{
 
+    int     [] chiffre={0,1,2,3,4,5,6,7,8,9};
+    String  [] alphabetsArray = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "!"};
+
     public String casserMotDePasse(String motDePasse){
+
+        
 
 
 
