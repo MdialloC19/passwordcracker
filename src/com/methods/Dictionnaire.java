c88036686dc3a323bca1b2760f3fc85f93fdc47f
Cheikh Gaye <gayec2270198@gmail.com>
2023-05-18 16:52:51 +0200

    Ajout du dictionnaire

--- src/com/methods/Dictionnaire.java
+++ src/com/methods/Dictionnaire.java
@@ -1,8 +1,30 @@
 package com.methods;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Dictionnaire  extends Methodes{
 
     public void casserMotDePasse(String motDePasse){
+        List<String> dictionary = new ArrayList<>();
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(new File("../../../dictionary/passwords")))) {
+            String ligne;
+
+            while ((ligne = reader.readLine()) != null) {
+                dictionary.add(ligne);
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+
+        for (String mot : dictionary) {
+            String hashMdp;
+        }
 
     }
     
