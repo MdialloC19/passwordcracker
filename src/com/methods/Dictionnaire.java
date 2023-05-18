9754a25e01dfecb688699dfb3acd9cbce61eaf9e
Cheikh Gaye <gayec2270198@gmail.com>
2023-05-18 18:07:58 +0200

    Methode par dictionnaire terminé

--- src/com/methods/Dictionnaire.java
+++ src/com/methods/Dictionnaire.java
@@ -3,17 +3,21 @@ package com.methods;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.hachages.HachageMd5;
 
 public class Dictionnaire  extends Methodes{
 
     public void casserMotDePasse(String motDePasse){
         List<String> dictionary = new ArrayList<>();
 
-        try(BufferedReader reader = new BufferedReader(new FileReader(new File("../../../dictionary/passwords")))) {
+        // on ouvre le fichier en especifiant son path et ensuite le lire ligne par ligne grace à la classe BufferedReader
+        try(BufferedReader reader = new BufferedReader(new FileReader(new File("../../../dictionary/passwords.txt")))) {
             String ligne;
-
+            // Chaque ligne lue est ajoutée à la liste dictionary
             while ((ligne = reader.readLine()) != null) {
                 dictionary.add(ligne);
             }
@@ -22,10 +26,20 @@ public class Dictionnaire  extends Methodes{
             e.printStackTrace();
         }
 
+        // on parcourt les éléments de la liste dictionnary et pour chaque elmt on compare son hash à celui fourni en paramétre 
         for (String mot : dictionary) {
-            String hashMdp;
+            try {
+                String hashMdp = HachageMd5.hachage("?", mot);
+                if (hashMdp.equals(motDePasse)) {
+                    System.out.println("Mot de passe trouvé: "+ mot);
+                    System.exit(0);
+                }
+            } catch (NoSuchAlgorithmException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }           
         }
-
+        System.out.println("Mot de passe non trouvé");
     }
     
 }
