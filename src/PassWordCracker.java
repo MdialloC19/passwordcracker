fe9838fbac28b3bddf42bb682ab591d0f3305e91
MdialloC19 <mdiallo3211@gmail.com>
2023-06-09 02:10:08 +0000

    Classe pour l'envoie de requete http terminer

--- src/PassWordCracker.java
+++ src/PassWordCracker.java
@@ -9,54 +9,44 @@ import com.methods.*;
 public class PassWordCracker {
     
     public static void main(String []args){
-        System.out.println("****** Programme pour casser un code de 5 à 6 chiffres par deux méthodes ********\n\t\t 1- Méthode par BRUTE FORCE ( taper bruteforce)\n\t\t 2- Méthode par DICTIONNAIRE (dictionnaire)");
+        System.out.println("****** Programme pour casser un code de 5 à 6 chiffres par deux méthodes ********\n\t\t"+
+        "1- Méthode par BRUTE FORCE \n\t\t"+" 2- Méthode par DICTIONNAIRE");
         
         Scanner sc=new Scanner(System.in);
-        String mdp1=sc.nextLine();
-
+        int choix=sc.nextInt();
+        String mdp1=new String();
+        switch (choix) {
+            case 1:
+            mdp1="bruteforce";
+                break;
+            case 2:
+            mdp1="dictionnaire";
+                break;
+            default:
+            System.out.println("Choix indisponible");
+                break;
+        }
         Methodes Method=FabriqueMethode.getInstance(mdp1);
-        System.out.println("Donner le mot de passe hacher à rechercher");
         
-        String mdp=sc.nextLine();
+        System.out.println("1-Trouver un mot de passe \n2-Trouver le mot de passe d'authentification");
+        int choix1=sc.nextInt();
+
+        if(choix==1){
+            System.out.println("Donner le mot de passe hacher à rechercher");
+            String mdp=sc.nextLine();
+            Method.casserMotDePasse(mdp);
+
+        }else{
+            
+
+        }
+        
 
         System.out.println("Veuillez patienter SVP");
 
-        Method.casserMotDePasse(mdp);
+      
 
     
-        // int passwordLength = 5; 
-        // //char[] password = new char[passwordLength];
-        // char password[]={'a','a','a', 'a','a'};
-       
-
-        // while (password[passwordLength - 1] <= 'z') {
-            
-        //     if(verification(mdp,new String(password))){
-        //         System.out.println("Voici le mot de passe correspondant "+new String(password));
-        //         break;
-        //     }
-        //     // Génération des combinaisons
-        //     int index = 0;
-        //     while (index < passwordLength) {
-        //         if (password[index] < 'z') {
-        //             password[index]++;
-        //             break;
-        //         } else {
-        //             password[index] = 'a';
-        //             index++;
-        //         }
-        //     }
-        // }
     }
-    // public static boolean verification(String mdp,String password){
-    //     HachageMd5 hash=new HachageMd5();
-    //     String actuMdp="";
-    //     try {
-    //         actuMdp = hash.hachage("MD5",password);
-    //     } catch (NoSuchAlgorithmException e) {
-    //         // TODO Auto-generated catch block
-    //         e.printStackTrace();
-    //     }
-    //     return mdp.equals(actuMdp);
-    // }
+  
 }
